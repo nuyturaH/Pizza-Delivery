818bf611d8aba7529063624d1a9ea387aa80bde3
nuyturaH <har8yun.khachatryan@gmail.com>
2023-01-25 00:37:50 +0400

    Initial commit with the skeleton of the project

---
+++ app/src/main/java/com/harutyun/pizzadelivery/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.harutyun.pizzadelivery.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.harutyun.pizzadelivery.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
