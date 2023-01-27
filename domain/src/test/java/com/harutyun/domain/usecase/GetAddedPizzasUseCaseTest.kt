31321db9cced2f72c8856bc9dc9674614a512ebe
nuyturaH <har8yun.khachatryan@gmail.com>
2023-01-27 05:24:47 +0400

    Added unit test for GetAddedPizzas use case

---
+++ domain/src/test/java/com/harutyun/domain/usecase/GetAddedPizzasUseCaseTest.kt
@@ -0,0 +1,40 @@
+package com.harutyun.domain.usecase
+
+import com.harutyun.domain.model.Pizza
+import com.harutyun.domain.model.PizzaSize
+import com.harutyun.domain.repository.PizzasRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class GetAddedPizzasUseCaseTest {
+
+    private val pizzasRepository = mock<PizzasRepository>()
+
+    @Test
+    fun `should return the same added pizzas as in repository`() {
+        val testAddedPizzas = listOf(Pizza("A", 1.1, PizzaSize.Full, true))
+        Mockito.`when`(pizzasRepository.getAddedPizzas()).thenReturn(testAddedPizzas)
+
+        val getAddedPizzasUseCase = GetAddedPizzasUseCase(pizzasRepository)
+
+        val actual = getAddedPizzasUseCase()
+        val expected = listOf(Pizza("A", 1.1, PizzaSize.Full, true))
+
+        Assertions.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should not return the same added pizzas as in repository`() {
+        val testAddedPizzas = listOf(Pizza("A", 1.1, PizzaSize.Full, true))
+        Mockito.`when`(pizzasRepository.getAddedPizzas()).thenReturn(testAddedPizzas)
+
+        val getAddedPizzasUseCase = GetAddedPizzasUseCase(pizzasRepository)
+
+        val actual = getAddedPizzasUseCase()
+        val expected = listOf(Pizza("B", 1.1, PizzaSize.Full, true))
+
+        Assertions.assertNotEquals(expected, actual)
+    }
+}
